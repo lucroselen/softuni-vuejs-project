15d8852cc544ff57c6b35da68d5f705d3fde8a24
lucroselen <mriliev@outlook.com>
2023-11-28 03:59:24 +0200

    Register page added

---
+++ vite-project-softuni/src/components/PageRegister.vue
@@ -0,0 +1,124 @@
+<script setup></script>
+
+<template>
+  <div class="registerContainer">
+    <div class="registerWrapper">
+      <div class="registerText">
+        <h2>REGISTER</h2>
+        <p>Let's get you on board!</p>
+      </div>
+      <form>
+        <input type="text" name="firstName" placeholder="First Name" />
+        <input type="text" name="lastName" placeholder="Last Name" />
+        <input type="email" name="email" placeholder="Email Address" />
+        <input type="password" name="password" placeholder="Password" />
+        <input
+          type="password"
+          name="rePassword"
+          placeholder="Repeat Password"
+        />
+        <button>Register</button>
+      </form>
+      <div class="formLink">
+        <span>Already have an account?</span>
+        <span><a href="/user/login">Sign In</a></span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.registerContainer {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  width: 100vw;
+  height: calc(100vh - (53px + 38px));
+  margin-top: 53px;
+  margin-bottom: 38px;
+}
+
+.registerContainer form {
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+}
+
+.registerContainer a,
+.registerContainer span {
+  color: #fff;
+  padding-inline: 0.25rem;
+}
+
+.registerContainer button {
+  margin: 1rem;
+}
+
+.registerWrapper {
+  border: #212529da solid 1px;
+  border-radius: 5%;
+  -webkit-box-shadow: 0px 10px 1000px -7px #000000,
+    0px 3px 12px 6px rgba(224, 224, 224, 0.2);
+  box-shadow: 0px 10px 1000px -7px #000000,
+    0px 3px 12px 6px rgba(224, 224, 224, 0.2);
+  background-color: #212529da;
+  padding: 1rem;
+  width: 450px;
+  min-width: 200px;
+  min-height: 620px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.registerText {
+  text-align: center;
+  color: #fff;
+  padding-bottom: 1.5rem;
+}
+
+.registerText p {
+  padding: 0 3rem;
+  color: hsla(0, 0%, 100%, 0.815);
+}
+.registerText h2 {
+  font-size: 30px;
+}
+
+.registerWrapper form input {
+  padding: 0.75rem 10rem 0.75rem 0.75rem;
+  border: unset;
+  border-radius: 4px;
+  margin: 0.2rem;
+  font-size: 18px;
+}
+.registerWrapper button {
+  background-color: #212529da;
+  border: 1px solid #fff;
+  border-radius: 4px;
+  color: #fff;
+  padding: 0.75rem;
+  font-size: 18px;
+  margin-bottom: 2rem;
+}
+
+.registerWrapper button:hover {
+  color: #212529da;
+  background-color: #fff;
+  transition: all 0.45s ease;
+}
+
+.formLink span:last-child {
+  font-weight: bold;
+}
+
+.formLink,
+.registerText p {
+  font-size: large;
+}
+
+.formLink span:first-child {
+  color: hsla(0, 0%, 100%, 0.815);
+}
+</style>
