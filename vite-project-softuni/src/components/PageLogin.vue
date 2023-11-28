1dd4a6b12469a0f9ac631adefd0061462c4fef78
lucroselen <mriliev@outlook.com>
2023-11-28 03:53:02 +0200

    login page added

---
+++ vite-project-softuni/src/components/PageLogin.vue
@@ -0,0 +1,128 @@
+<script setup></script>
+
+<template>
+  <div class="loginContainer">
+    <div class="loginWrapper">
+      <div class="loginText">
+        <h2>LOGIN</h2>
+        <p>Glad to see you back! Please log-in to your account.</p>
+      </div>
+      <form>
+        <input
+          type="email"
+          name="email"
+          formControlName="email"
+          placeholder="Email Address"
+        />
+        <input
+          type="password"
+          name="password"
+          formControlName="password"
+          placeholder="Password"
+        />
+        <button type="submit">Login</button>
+      </form>
+      <div class="formLink">
+        <span>Don't have an account?</span>
+        <span><a href="/user/register">Sign Up</a></span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.loginContainer {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  width: 100vw;
+  height: calc(100vh - (53px + 38px));
+  margin-top: 53px;
+  margin-bottom: 38px;
+}
+
+.loginContainer form {
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+}
+
+.loginContainer a,
+.loginContainer span {
+  color: #fff;
+  padding-inline: 0.25rem;
+}
+
+.loginContainer button {
+  margin: 1rem;
+}
+
+.loginWrapper {
+  border: #212529da solid 1px;
+  border-radius: 5%;
+  -webkit-box-shadow: 0px 10px 1000px -7px #000000,
+    0px 3px 12px 6px rgba(224, 224, 224, 0.2);
+  box-shadow: 0px 10px 1000px -7px #000000,
+    0px 3px 12px 6px rgba(224, 224, 224, 0.2);
+  background-color: #212529da;
+  padding: 1rem;
+  width: 450px;
+  height: 500px;
+  min-width: 200px;
+  min-height: 350px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.loginText {
+  text-align: center;
+  color: #fff;
+  padding-bottom: 1.5rem;
+}
+
+.loginText p {
+  padding: 0 3rem;
+  color: hsla(0, 0%, 100%, 0.815);
+}
+.loginText h2 {
+  font-size: 30px;
+}
+
+.loginWrapper form input {
+  padding: 0.75rem 10rem 0.75rem 0.75rem;
+  border: unset;
+  border-radius: 4px;
+  margin: 0.2rem;
+  font-size: 18px;
+}
+.loginWrapper button {
+  background-color: #212529da;
+  border: 1px solid #fff;
+  border-radius: 4px;
+  color: #fff;
+  padding: 0.75rem;
+  font-size: 18px;
+  margin-bottom: 2rem;
+}
+
+.loginWrapper button:hover {
+  color: #212529da;
+  background-color: #fff;
+  transition: all 0.45s ease;
+}
+
+.formLink span:last-child {
+  font-weight: bold;
+}
+
+.formLink,
+.loginText p {
+  font-size: large;
+}
+
+.formLink span:first-child {
+  color: hsla(0, 0%, 100%, 0.815);
+}
+</style>
