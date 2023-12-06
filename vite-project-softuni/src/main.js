859779e842f5e1696bcbf5b9b71824231afb508a
lucroselen <mriliev@outlook.com>
2023-12-07 01:35:20 +0200

    pinia added

--- vite-project-softuni/src/main.js
+++ vite-project-softuni/src/main.js
@@ -1,12 +1,19 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 import "./styles/reset.css";
 import "./style.css";
 import App from "./App.vue";
 import router from "./router";
 import VueGoogleMaps from "@fawmi/vue-google-maps";
+import { useUserStore } from "./store/userStore";
 
 const app = createApp(App);
+const pinia = createPinia();
 app.use(router);
+
+app.use(pinia);
+const userStore = useUserStore();
+userStore.getPersistedProfile();
 app.use(VueGoogleMaps, {
   load: {
     key: "AIzaSyCfdy2keDRAnX2JJLw9WwIISXpheNlFHVA",
