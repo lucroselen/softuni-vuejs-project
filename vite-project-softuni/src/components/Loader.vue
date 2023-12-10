ccdda119f7123afa700d17fa2b2d86893bf96f3c
lucroselen <mriliev@outlook.com>
2023-12-10 02:54:04 +0200

    add extremely fancy loader

---
+++ vite-project-softuni/src/components/Loader.vue
@@ -0,0 +1,224 @@
+<template>
+  <div class="center">
+    <figure class="loader">
+      <div class="car">
+        <span class="body"></span>
+        <span class="wheels"></span>
+      </div>
+      <div class="strikes">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </figure>
+  </div>
+</template>
+
+<style scoped>
+.center {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+body {
+  margin: 0;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #333;
+}
+
+.loader {
+  width: 11.7em;
+  height: 4.2em;
+  font-size: 20px;
+  position: relative;
+  color: lightcyan;
+}
+
+.car {
+  position: absolute;
+  width: inherit;
+  height: 2em;
+  background-color: currentColor;
+  top: 1.5em;
+  border-radius: 0 5em 1em 0 / 0 4em 1em 0;
+  animation: run 0.25s linear infinite;
+  margin: 0 auto;
+}
+
+@keyframes run {
+  0% {
+    transform: translate(0.2em, 0.1em);
+  }
+
+  20% {
+    transform: translate(0.1em, 0.2em);
+  }
+
+  40% {
+    transform: translate(0.1em, -0.1em);
+  }
+
+  60% {
+    transform: translate(-0.1em, 0.2em);
+  }
+
+  80% {
+    transform: translate(-0.1em, 0.1em);
+  }
+
+  100% {
+    transform: translate(0.2em, 0.1em);
+  }
+}
+
+.car::before {
+  content: "";
+  position: absolute;
+  width: 0;
+  height: 0;
+  border: 0.6em solid transparent;
+  top: -0.5em;
+  border-left-width: 0;
+  border-right-color: currentColor;
+  transform-origin: left;
+  transform: rotate(-45deg);
+}
+
+.body {
+  position: absolute;
+  width: 7.5em;
+  height: 3.5em;
+  box-sizing: border-box;
+  border: 0.4em solid;
+  top: -1.5em;
+  left: 1.2em;
+  border-radius: 3em 4.5em 0 0 / 3em 4em 0 0;
+}
+
+.body::before {
+  content: "";
+  position: absolute;
+  width: 3.5em;
+  height: inherit;
+  background-color: currentColor;
+  border-top-left-radius: inherit;
+  top: -0.4em;
+  left: -0.4em;
+}
+
+.wheels::before,
+.wheels::after {
+  content: "";
+  position: absolute;
+  box-sizing: border-box;
+  width: 2.6em;
+  height: 2.6em;
+  background-color: #333;
+  border-radius: 50%;
+  bottom: -1em;
+  border: 0.3em solid #333;
+  background-image: linear-gradient(
+      135deg,
+      transparent 45%,
+      currentColor 46%,
+      currentColor 54%,
+      transparent 55%
+    ),
+    linear-gradient(
+      90deg,
+      transparent 45%,
+      currentColor 46%,
+      currentColor 54%,
+      transparent 55%
+    ),
+    linear-gradient(
+      45deg,
+      transparent 45%,
+      currentColor 46%,
+      currentColor 54%,
+      transparent 55%
+    ),
+    linear-gradient(
+      0deg,
+      transparent 45%,
+      currentColor 46%,
+      currentColor 54%,
+      transparent 55%
+    ),
+    radial-gradient(
+      currentColor 29%,
+      transparent 30%,
+      transparent 50%,
+      currentColor 51%
+    );
+  animation: rotating 0.5s linear infinite;
+}
+
+.wheels::before {
+  left: 1.2em;
+}
+
+.wheels::after {
+  right: 0.8em;
+}
+
+@keyframes rotating {
+  to {
+    transform: rotate(1turn);
+  }
+}
+
+.strikes {
+  position: absolute;
+  width: 1em;
+  height: inherit;
+  left: -1em;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.strikes span {
+  height: 0.1em;
+  background-color: lightcyan;
+  animation: drift 0.2s linear infinite;
+  animation-delay: calc(var(--n) * -0.05s);
+}
+
+@keyframes drift {
+  from {
+    transform: translate(3.5em);
+  }
+
+  to {
+    transform: translate(-8em);
+    filter: opacity(0);
+  }
+}
+
+.strikes span:nth-child(1) {
+  --n: 1;
+}
+
+.strikes span:nth-child(2) {
+  --n: 2;
+}
+
+.strikes span:nth-child(3) {
+  --n: 3;
+}
+
+.strikes span:nth-child(4) {
+  --n: 4;
+}
+
+.strikes span:nth-child(5) {
+  --n: 5;
+}
+</style>
