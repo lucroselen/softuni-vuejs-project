54d3a6b336cfc2ab96981e370add0640eb60a45b
lucroselen <mriliev@outlook.com>
2023-12-11 06:18:09 +0200

    create getCar http request

--- cars-eu/src/dataProviders/cars.js
+++ cars-eu/src/dataProviders/cars.js
@@ -9,6 +9,15 @@ export async function getCars(type) {
     return null;
   }
 }
+export async function getCar(id) {
+  try {
+    const res = await axiosInstance.get(`/details/${id}`);
+    return res.data;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
 
 export async function addCar(carData) {
   try {
