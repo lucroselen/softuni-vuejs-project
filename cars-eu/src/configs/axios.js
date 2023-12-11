f359727d1b230d08a40d8ec0d93de5ed7abc7ea5
lucroselen <mriliev@outlook.com>
2023-12-12 00:35:21 +0200

    remove .env file, use hardcoded baseURL

--- cars-eu/src/configs/axios.js
+++ cars-eu/src/configs/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_APP_API,
+  baseURL: "http://localhost:5000",
   withCredentials: true,
 });
 
